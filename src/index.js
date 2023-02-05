d81ddfbdcda1d54657dcfcbaa4898431672e5d77
wxhyxtrx <banyuwangi1999@gmail.com>
2023-02-05 12:23:34 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import store from "./store";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const client = new QueryClient();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <QueryClientProvider client={client}>
+        <App />
+      </QueryClientProvider>
+    </Provider>
+  </React.StrictMode>
+);
+
+reportWebVitals();
